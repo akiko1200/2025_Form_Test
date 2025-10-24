54cdf1b3203a7c2ffc680af09c0bce814785c1bf
akiko1200 <acco.chan.1200.s@gmail.com>
2025-10-24 15:36:26 +0900

    練習問題1,2(24個のボタン)

--- Form_Test/Form_Test/Form1.cs
+++ Form_Test/Form_Test/Form1.cs
@@ -15,6 +15,30 @@ namespace Form_Test
         public Form1()
         {
             InitializeComponent();
+
+            for (int i = 0; i < 4; i++) {
+                for (int j = 0; j < 6; j++) {
+                    // インスタンスの作成
+                    TestButton testButton = new TestButton();
+
+                    // ボタンの位置を設定
+                    testButton.Location = new Point(50 * j, 50 * i);
+
+                    // ボタンの大きさを設定
+                    testButton.Size = new Size(50, 50);
+
+                    // ボタン内のテキストを設定
+                    testButton.Text = "";
+
+                    // コントロールにボタンを追加
+                    Controls.Add(testButton);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("C#の世界へようこそ！");
         }
     }
 }
---
+++ Form_Test/Form_Test/TestButton.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Form_Test
+{
+    // Buttonクラスを継承したTestButton
+    internal class TestButton : Button
+    {
+
+    }
+}
